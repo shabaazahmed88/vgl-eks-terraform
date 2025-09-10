52c83b62614ee238565dcec5c5cf6ce28edab426
mohammedm <mohammedm834@ninjacart.com>
2025-09-10 22:21:52 +0530

    ksllsl

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -48,14 +48,6 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.this.token
 }
 
-provider "helm" {
-  alias = "eks"
-  kubernetes {
-    host                   = data.aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.this.token
-  }
-}
 
 module "addons" {
   source = "../../modules/eks-addons"
